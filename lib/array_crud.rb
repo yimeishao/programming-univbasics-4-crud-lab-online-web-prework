471398bba69a061620d7ceadc2870ce2beda012d
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-05 01:00:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- create_an_array.push("you")
+ us.push("yourself")
 end
 
 def add_element_to_start_of_array(array, element)
