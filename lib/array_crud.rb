c43978d20398ed6556194fff828cbdcaaf1786ec
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-05 01:14:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  us = ["me", "myself", "i", "you"]
- us >> "yourself"
+ us << "yourself"
 end
 
 def add_element_to_start_of_array(array, element)
